87663dc79ed829ffcc0df6069cfe21ec7d4ab059
olbrew <olbrew@users.noreply.github.com>
2019-11-26 01:23:01 +0100

    Fix deploy script to properly check network connection

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
 
-echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+echo -e "GET http://google.com HTTP/1.0\n\n" | nc google.com 80 > /dev/null 2>&1
 
-# Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+# Do we have a working internet connection?
+if [ $? -eq 0 ];
+ then
+    echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
-# Go To Public folder
-cd public
-# Add changes to git.
-git add .
+    # Build the project.
+    hugo
 
-# Commit changes.
-msg="rebuilding site `date`"
-if [ $# -eq 1 ]
-  then msg="$1"
+    # Go To Public folder.
+    cd public
+    # Add changes to git.
+    git add .
+
+    # Commit changes with default or provided commit title.
+    msg="New site build: `date`"
+    if [ $# -eq 1 ]
+      then msg="$1"
+    fi
+    git commit -m "$msg"
+
+    # Push source and build repos.
+    git push origin master
+
+    # Come Back up to the Project Root.
+    cd ..
+  else
+    echo "No internet connection found. Aborting for now."
 fi
-git commit -m "$msg"
-
-# Push source and build repos.
-git push origin master
-
-# Come Back up to the Project Root
-cd ..
